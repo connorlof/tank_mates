bbd2692f5d84a3b6e636ac654a9961b219adf355
Connor Loftus <connorlof@gmail.com>
2021-08-10 08:45:01 -0400

    Add separate class for filter view

--- lib/ui/screens/add_fish_screen.dart
+++ lib/ui/screens/add_fish_screen.dart
@@ -3,18 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tank_mates/bloc/edit_tank_view_model.dart';
 import 'package:tank_mates/data/model/species.dart';
+import 'package:tank_mates/ui/screens/filter_list_screen.dart';
 import 'package:tank_mates/util/constants.dart';
 
 class AddFishScreen extends StatelessWidget {
   AddFishScreen({@required this.data});
 
   final List<Species> data;
+  final filterIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final viewModel = Provider.of<EditTankViewModel>(context, listen: false);
-    List<Species> activeData =
-        data.where((species) => species.speciesGroup == 'angelfish').toList();
+    var speciesGroups = ['All'] + viewModel.speciesGroups();
+    List<Species> activeData = data.where((species) {
+      if (filterIndex == 0)
+        return true;
+      else
+        return species.speciesGroup == speciesGroups[filterIndex];
+    }).toList();
 
     return Container(
       color: Color(0xff757575),
@@ -40,7 +47,17 @@ class AddFishScreen extends StatelessWidget {
                 ),
                 GestureDetector(
                   onTap: () {
-                    _showFilterDialog(context);
+                    showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      builder: (context) => SingleChildScrollView(
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).viewInsets.bottom),
+                          child: FilterListScreen(),
+                        ),
+                      ),
+                    );
                   },
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
@@ -108,76 +125,5 @@ class AddFishScreen extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void _showFilterDialog(BuildContext context) {
-    final viewModel = Provider.of<EditTankViewModel>(context, listen: false);
-    final speciesGroups = viewModel.speciesGroups();
-
-    showDialog(
-        context: context,
-        builder: (context) {
-          return StatefulBuilder(builder: (context, setState) {
-            return AlertDialog(
-                title: Text(
-                  'Filter by Species Type',
-                  textAlign: TextAlign.center,
-                  style: kTextStyleHeader,
-                ),
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SingleChildScrollView(
-                      child: Container(
-                        width: double.minPositive,
-                        height: 400.0,
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: speciesGroups.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: <Widget>[
-                                Divider(),
-                                CheckboxListTile(
-                                  value: false,
-                                  title: Text(
-                                    '${speciesGroups[index]}',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontFamily: 'Oswald',
-                                      fontSize: 18.0,
-                                      color: Color(0xFF3c4146),
-                                    ),
-                                  ),
-                                  onChanged: (val) {},
-                                )
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    TextButton(
-                      child: Text(
-                        'Accept',
-                        style: TextStyle(
-                          color: kBackGroundColor,
-                          fontFamily: 'Oswald',
-                          fontSize: 18.0,
-                        ),
-                      ),
-                      style:
-                          TextButton.styleFrom(backgroundColor: kPrimaryColor),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                ));
-          });
-        });
   }
 }
---
+++ lib/ui/screens/filter_list_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:tank_mates/util/constants.dart';
+
+class FilterListScreen extends StatefulWidget {
+  FilterListScreen();
+
+  @override
+  _FilterListScreenState createState() => _FilterListScreenState();
+}
+
+class _FilterListScreenState extends State<FilterListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var filterIndex = 0;
+    var speciesGroups = ['All'];
+
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: kBackGroundColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Filter by Species Group',
+              textAlign: TextAlign.center,
+              style: kTextStyleHeader,
+            ),
+            Container(
+              height: 400.0,
+              child: ListView.builder(
+                itemCount: speciesGroups.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Divider(),
+                      RadioListTile(
+                        value: index,
+                        groupValue: filterIndex,
+                        title: Text(
+                            toBeginningOfSentenceCase(speciesGroups[index])),
+                        onChanged: (val) {
+                          setState(() {
+                            filterIndex = val;
+                          });
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            TextButton(
+              child: Text(
+                'Filter',
+                style: TextStyle(
+                  color: kBackGroundColor,
+                  fontFamily: 'Oswald',
+                  fontSize: 18.0,
+                ),
+              ),
+              style: TextButton.styleFrom(backgroundColor: kPrimaryColor),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
